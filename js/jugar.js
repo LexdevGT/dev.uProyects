8d8d9948418bd61bb78870064c802eb34cc50ed2
marv-dev86 <marvine.alvarado@gmail.com>
2021-02-01 20:05:06 -0600

    01/02/2021 8:03 pm
    jugar.html v2 resposinve y funcionando carton 1, jugar.css para separar los estilos del resto de las paginas

---
+++ js/jugar.js
@@ -0,0 +1,86 @@
+const Numeros = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63]
+
+const cartonUno = document.getElementById('carton1')
+const cartonDos = document.getElementById('carton2')
+const cartonTres = document.getElementById('carton3')
+const fragmentUno = document.createDocumentFragment()
+const fragmentDos = document.createDocumentFragment()
+const fragmentTres = document.createDocumentFragment()
+
+for(const numero of Numeros){
+    const cartonItem = document.createElement("a")
+    cartonItem.textContent = numero
+    cartonItem.setAttribute('value',numero)
+    cartonItem.classList.add("btn","btn-light","rounded-circle",'cartonItem','bg-unselected')
+    fragmentUno.appendChild(cartonItem)
+       
+}
+
+    cartonUno.appendChild(fragmentUno)
+
+for(const numero of Numeros){
+    const cartonItem = document.createElement("a")
+    cartonItem.textContent = numero
+    cartonItem.setAttribute('value',numero)
+    cartonItem.classList.add('btn', 'rounded-circle','cartonItem','bg-unselected' )
+    fragmentDos.appendChild(cartonItem)
+       
+}
+
+    cartonDos.appendChild(fragmentDos)
+
+
+for(const numero of Numeros){
+    const cartonItem = document.createElement("a")
+    cartonItem.textContent = numero
+    cartonItem.setAttribute('value',numero)
+    cartonItem.classList.add('btn', 'rounded-circle','cartonItem','bg-unselected')
+    fragmentTres.appendChild(cartonItem)
+       
+}
+
+    cartonTres.appendChild(fragmentTres)
+
+//agregando los numeros seleccionados por carton 
+
+const cartonUnoSelect = ["--","--","--","--"]
+let cartonUnoItem1 = document.getElementById('cartonUnoItem1')
+let cartonUnoItem2 = document.getElementById('cartonUnoItem2')
+let cartonUnoItem3 = document.getElementById('cartonUnoItem3')
+let cartonUnoItem4 = document.getElementById('cartonUnoItem4')
+let i = 0
+ cartonUno.addEventListener('click', (e)=>{
+    
+        if(i< 4){
+            // cartonUnoSelect.shift()
+            let valueItem = e.target.textContent
+            e.target.classList.replace('bg-unselected','bg-selected')
+            cartonUnoSelect.unshift(valueItem)
+            // console.log(cartonUnoSelect)
+            
+           i++
+           cartonUnoSelect.pop()
+            cartonUnoItem1.value = cartonUnoSelect[3]
+            
+            cartonUnoItem2.value = cartonUnoSelect[2] 
+            cartonUnoItem3.value = cartonUnoSelect[1] 
+            cartonUnoItem4.value = cartonUnoSelect[0]
+
+            console.log(cartonUnoSelect)
+
+        } 
+    })
+    
+    
+    
+         
+   
+
+
+
+  
+
+    
+   
+
+   
