6da82eab54dfbf0abd9e7501d8a4ccb42c183e5f
marv-dev86 <marvine.alvarado@gmail.com>
2021-02-09 18:57:56 -0600

    09/02/2021 6:57 pm
    la opcion de agregar hasta 10 cartones es disponible ya, y es responsive

--- js/jugar2.js
+++ js/jugar2.js
@@ -2,7 +2,7 @@ $(function(){
 
     cartonUno()
     mySelection()
-    // ballNumber()
+    agregarCarton()
 
 })
 
@@ -61,6 +61,63 @@ function mySelection(){
 
     })
 }//funcion myselection
+
+
+function agregarCarton(){
+
+
+    let  i = 2 //contara el numero de cartones por agregar hasta 10
+
+    $('.carton_nuevo').click( function(){
+
+      
+        if(i<=10){
+            let CartonNumero        = `<div class="carton${i} hover"> </div>`//father
+            let cartonEncabezado    = `<h3 class="text-center bg-warning rounded ">Carton ${i}</h3>`//hijo 1 encabezado
+
+            let cartonContenedor    = `<div class="carton  border border-1 pt-1 pb-1 rounded" id=carton${i} class="d-flex"> </div>`//hijo 2 contenedor de las balls
+
+            let row                 = `<div class="d-flex justify-content-evenly p-2" id="resultado${i}"></div> `//hijo 3
+
+            
+            $('.cartones').append(CartonNumero)
+            $(`.carton${i}`).append(cartonEncabezado, cartonContenedor, row)
+
+            let balls = 1
+    
+            while(balls <= 63){
+            
+                let cartonItem = $(`#carton${i}`).append(`<div id="${balls}"> ${balls} </div>`).find('div:last').addClass("rounded-circle"+" " +"cartonItem"+" "+'bg-unselected'+" "+ "d-flex"+" "+ "align-items-center"+" "+ " justify-content-center").attr(`id, ${balls}`)//las pelotas podrian necesitar un id por cada carton 
+            
+                balls++
+            } 
+
+
+            let resultadoItem = 0
+
+            while(resultadoItem <4){
+                $(`#resultado${i}`).append(`<div class="${i}${resultadoItem} circle p-2" ></div>`) //aca sera posible agrear una clase especial par identificar el div que contendra el numero selecionado
+
+                resultadoItem ++
+            }
+
+
+            
+            
+            
+            i++
+            console.log(i)
+        }else{
+            alert('Has llegado al maximo numero de cartones')
+        }
+      
+            
+        
+
+    }) // click
+
+
+}//funcion agregar carton
 
 
 
